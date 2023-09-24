7de2133afe8254324ed039b23570fa1a1b401e5b
mohammadarifsojib <67918517+mohammadarifsojib@users.noreply.github.com>
2023-09-24 16:36:03 +0600

    Uninstall Splidejs

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueSplide from "@splidejs/vue-splide";
 
 const app = createApp(App);
 
-app.use(router, VueSplide);
+app.use(router);
 
 app.mount("#app");
